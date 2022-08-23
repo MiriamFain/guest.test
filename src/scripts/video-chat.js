5b678c7a26d80c696b314e25107ee097e752989d
vitalii-khymchuk <vitalii.workbox001@gmail.com>
2022-08-23 15:14:34 +0300

    add video chat

--- src/scripts/video-chat.js
+++ src/scripts/video-chat.js
@@ -5,23 +5,33 @@ const body = document.querySelector('body');
 const frameWrapper = document.querySelector('.frame__wrapper');
 const backdrop = document.querySelector('.videoChatBackdrop');
 const openChatBtn = document.querySelector('.openChatBtn');
+const closeChatBtn = document.querySelector('.closeChatBtn');
 
 fullScreenBtn.addEventListener('click', enableFullScreen);
 windowModeBtn.addEventListener('click', disableFullScreen);
 window.addEventListener('keydown', disableFullScreen);
 openChatBtn.addEventListener('click', openChat);
+closeChatBtn.addEventListener('click', closeChat);
 
 function enableFullScreen() {
   frameWrapper.classList.add('fullScreen');
-  frame.scrollIntoView();
-  body.classList.add('no-scroll');
+  // frame.scrollIntoView();
 }
 
 function disableFullScreen(event) {
   if (event.code === 'Escape' || event.isTrusted) {
     frameWrapper.classList.remove('fullScreen');
-    body.classList.remove('no-scroll');
   }
+}
+
+function openChat() {
+  backdrop.classList.remove('is-hidden');
+  body.classList.add('no-scroll');
+}
+
+function closeChat() {
+  backdrop.classList.add('is-hidden');
+  body.classList.remove('no-scroll');
 }
 
 // function getValue(event) {
@@ -30,7 +40,3 @@ function disableFullScreen(event) {
 //   frame.src = `https://imind.com/ru/r/${room}`;
 //   setTimeout(showWindow, 2000);
 // }
-
-function openChat() {
-  videoChatBackdrop.classList.toggle('is-hidden');
-}
