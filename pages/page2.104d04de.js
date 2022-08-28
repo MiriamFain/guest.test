8b6a18f3c91da3d3cc070e7b002c9dc24fedc8e6
miriamFain <104686417+MiriamFain@users.noreply.github.com>
2022-08-28 14:07:27 +0300

    create room 2

--- pages/page2.104d04de.js
+++ pages/page2.104d04de.js
@@ -1,2 +1,25 @@
-document.querySelector(".frame");const e=document.querySelector(".fullScreenBtn"),n=document.querySelector(".windowModeBtn"),t=document.querySelector("body"),c=document.querySelector(".frame__wrapper"),o=document.querySelector(".videoChatBackdrop"),d=document.querySelector(".openChatBtn"),r=document.querySelector(".closeChatBtn");function s(e){("Escape"===e.code||e.isTrusted)&&c.classList.remove("fullScreen")}e.addEventListener("click",(function(){c.classList.add("fullScreen")})),n.addEventListener("click",s),window.addEventListener("keydown",s),d.addEventListener("click",(function(){o.classList.remove("is-hidden"),t.classList.add("no-scroll")})),r.addEventListener("click",(function(){o.classList.add("is-hidden"),t.classList.remove("no-scroll"),c.innerHTML=c.innerHTML}));
+document.querySelector(".frame");
+const e = document.querySelector(".fullScreenBtn"),
+  n = document.querySelector(".windowModeBtn"),
+  t = document.querySelector("body"),
+  c = document.querySelector(".frame__wrapper"),
+  o = document.querySelector(".videoChatBackdrop"),
+  d = document.querySelector(".openChatBtn"),
+  r = document.querySelector(".closeChatBtn");
+function s(e) {
+  ("Escape" === e.code || e.isTrusted) && c.classList.remove("fullScreen");
+}
+e.addEventListener("click", function () {
+  c.classList.add("fullScreen");
+}),
+  n.addEventListener("click", s),
+  window.addEventListener("keydown", s),
+  d.addEventListener("click", function () {
+    o.classList.remove("is-hidden"), t.classList.add("no-scroll");
+  }),
+  r.addEventListener("click", function () {
+    o.classList.add("is-hidden"),
+      t.classList.remove("no-scroll"),
+      (c.innerHTML = c.innerHTML);
+  });
 //# sourceMappingURL=page2.104d04de.js.map
--- pages/page2.9f3324e1.js
+++ pages/page2.9f3324e1.js
@@ -1,2 +1,27 @@
-!function(){document.querySelector(".frame");var e=document.querySelector(".fullScreenBtn"),n=document.querySelector(".windowModeBtn"),t=document.querySelector("body"),c=document.querySelector(".frame__wrapper"),o=document.querySelector(".videoChatBackdrop"),r=document.querySelector(".openChatBtn"),d=document.querySelector(".closeChatBtn");function s(e){("Escape"===e.code||e.isTrusted)&&c.classList.remove("fullScreen")}e.addEventListener("click",(function(){c.classList.add("fullScreen")})),n.addEventListener("click",s),window.addEventListener("keydown",s),r.addEventListener("click",(function(){o.classList.remove("is-hidden"),t.classList.add("no-scroll")})),d.addEventListener("click",(function(){o.classList.add("is-hidden"),t.classList.remove("no-scroll"),c.innerHTML=c.innerHTML}))}();
+!(function () {
+  document.querySelector(".frame");
+  var e = document.querySelector(".fullScreenBtn"),
+    n = document.querySelector(".windowModeBtn"),
+    t = document.querySelector("body"),
+    c = document.querySelector(".frame__wrapper"),
+    o = document.querySelector(".videoChatBackdrop"),
+    r = document.querySelector(".openChatBtn"),
+    d = document.querySelector(".closeChatBtn");
+  function s(e) {
+    ("Escape" === e.code || e.isTrusted) && c.classList.remove("fullScreen");
+  }
+  e.addEventListener("click", function () {
+    c.classList.add("fullScreen");
+  }),
+    n.addEventListener("click", s),
+    window.addEventListener("keydown", s),
+    r.addEventListener("click", function () {
+      o.classList.remove("is-hidden"), t.classList.add("no-scroll");
+    }),
+    d.addEventListener("click", function () {
+      o.classList.add("is-hidden"),
+        t.classList.remove("no-scroll"),
+        (c.innerHTML = c.innerHTML);
+    });
+})();
 //# sourceMappingURL=page2.9f3324e1.js.map
